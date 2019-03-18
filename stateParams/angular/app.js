9074b4a01663a9ab54fbb3ef5dcd7d13effc2ea6
collero7 <collerortiz@gmail.com>
2019-03-18 20:21:46 +0100

    Segundo commit

--- stateParams/angular/app.js
+++ stateParams/angular/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('promesaApp', ['promesaApp.controladores']);
+var app = angular.module('promesaApp', ['promesaApp.controladores', 'ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/main');
+
+	$stateProvider
+		.state('main',
+		 {
+		 	url: '/main',
+		 	template: '<h2>Saludo al menú principal</h2><p>Este es un parrafo sencillo</p>'
+		 })
+		.state('usuarios',
+		 {
+		 	url: '/users',
+		 	template: '<h2>Modificacion de usuarios</h2><a ui-sref="usuarios.listado">Mostrar listado de usuarios</a><div ui-view></div>'
+		 })
+		.state('usuarios.listado',
+		 {
+		 	url: '/all',
+		 	template: '<ul><li>Cristopher</li><li>Paco</li><li>Manolo</li></ul>'
+		 });
+		
+}])
