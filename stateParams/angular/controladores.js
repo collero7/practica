80011976707923bc3b8c44f0d1d05de8d37da16f
collero7 <collerortiz@gmail.com>
2019-03-18 20:00:07 +0100

    Primer commit

---
+++ stateParams/angular/controladores.js
@@ -0,0 +1,45 @@
+var app = angular.module('promesaApp.controladores', []);
+
+app.controller('mainCtrl', ['$scope', '$q', function($scope, $q){
+
+	$scope.miVar = 0;
+
+
+	$scope.sumar = function( num ){
+
+		var q = $q.defer();
+		var valido = true;
+
+		num ++;
+		
+		setTimeout(function(){
+
+			if(true){
+
+				q.resolve(num);
+			}
+			else{
+				q.reject("Se ha producido un error");
+			}
+
+		}, 2000);
+			
+		return q.promise;
+	}
+
+
+	$scope.promise = $scope.sumar(1);
+
+	$scope.promise.then(
+		function(valor){
+			console.log("Promesa cumplida");
+			$scope.miVar = valor;
+		},
+		function(error){
+			console.error("Errorrr")
+		});
+
+
+
+	
+}])
